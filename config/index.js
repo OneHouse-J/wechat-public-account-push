7f9b68a159455aafd9ff0c11bc36db72126569f3
OneHouse-J <93515829+OneHouse-J@users.noreply.github.com>
2022-09-20 22:18:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -87,7 +87,7 @@ export const config = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        // {"keyword": "love_day", date: "2020-02-14"},
+        {"keyword": "love_day", date: "2020-02-14"},
         // 结婚纪念日
         // {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
@@ -200,11 +200,11 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"},
+    // {"keyword": "love_day", date: "2022-09-08"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2022-09-09"},
+    // {"keyword": "marry_day", date: "2022-09-09"},
     // 退伍日
-    {"keyword": "ex_day", date: "2022-09-10"},
+    // {"keyword": "ex_day", date: "2022-09-10"},
     // 你可以不断按格式往下增加
     // ...
   ],
